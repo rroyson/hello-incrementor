7c317bb3610681283afb07d18c2536afbb8d1612
rroyson <frontiersons@gmail.com>
2017-06-15 11:54:42 -0400

    routed home page

---
+++ src/components/title.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+/*
+function Title(props) {
+  return <h1>{props.children}</h1>
+}
+export default Title
+*/
+
+export default props => <h1 className="avenir">{props.childrent}</h1>
